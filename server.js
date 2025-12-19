3f9f95a40e79779ae81fa2bc4c9d03e036b75f97
SONALI-Nikki <sonalipriyardarshini@gmail.com>
2025-12-19 18:50:19 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import { connectDB } from './src/config/db.js';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import routes from './src/routes/index.js';
+import express from "express";
+import { connectDB } from "./src/config/db.js";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import routes from "./src/routes/index.js";
 import skillRoutes from "./src/routes/skillRoutes.js";
 import projectRoutes from "./src/routes/projectRoutes.js";
 import blogRoutes from "./src/routes/blogRoutes.js";
@@ -12,48 +12,49 @@ import testimonialRoutes from "./src/routes/testimonialRoutes.js";
 import serviceRoutes from "./src/routes/serviceRoutes.js";
 import aboutRoutes from "./src/routes/aboutRoutes.js";
 import authroutes from "./src/routes/auth.routes.js";
-import { seedAdmin } from './seedAdmin.js';
-import contactRoutes from './src/routes/contactRoutes.js';
+import { seedAdmin } from "./seedAdmin.js";
+import contactRoutes from "./src/routes/contactRoutes.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 // Middleware
-app.use(cors({
-  origin: ["http://localhost:5173",
-    "portfolio-cms-client-6d2b-a6xh72fgy-sonali-nikkis-projects.vercel.app"
-  ],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://portfolio-cms-client-6d2b-a6xh72fgy-sonali-nikkis-projects.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+app.options("*", cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-
 
 app.get("/", (req, res) => {
   res.send("Portfolio CMS Backend is running 🚀");
 });
 
-
 // Routes
-app.use('/api', routes);
+app.use("/api", routes);
 app.use("/api/auth", authroutes);
-app.use("/api/cms/about", aboutRoutes);   
-app.use("/api/cms/skills", skillRoutes);         
-app.use("/api/cms/projects", projectRoutes);       
-app.use("/api/cms/blogs", blogRoutes);            
-app.use("/api/cms/experience", experienceRoutes);  
-app.use("/api/cms/testimonials", testimonialRoutes); 
-app.use("/api/cms/services", serviceRoutes);       
+app.use("/api/cms/about", aboutRoutes);
+app.use("/api/cms/skills", skillRoutes);
+app.use("/api/cms/projects", projectRoutes);
+app.use("/api/cms/blogs", blogRoutes);
+app.use("/api/cms/experience", experienceRoutes);
+app.use("/api/cms/testimonials", testimonialRoutes);
+app.use("/api/cms/services", serviceRoutes);
 app.use("/uploads", express.static("uploads"));
-app.use('/api/cms/contact', contactRoutes);
-
+app.use("/api/cms/contact", contactRoutes);
 
 // Database connection
 connectDB();
-
 
 await seedAdmin();
 
