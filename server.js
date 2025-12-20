5645fb204d722cdfc400239a66b14c831e78013f
SONALI-Nikki <sonalipriyardarshini@gmail.com>
2025-12-20 15:19:09 +0530

    initial commit

--- server.js
+++ server.js
@@ -18,16 +18,22 @@ import contactRoutes from "./src/routes/contactRoutes.js";
 dotenv.config();
 const app = express();
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://portfolio-cms-client-jr39tgxb2-sonali-nikkis-projects.vercel.app"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
+  const origin = req.headers.origin;
+
+  if (
+    origin &&
+    (origin.includes("vercel.app") ||
+      origin === "http://localhost:5173")
+  ) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.header(
+  res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, OPTIONS"
   );
@@ -35,6 +41,7 @@ app.use((req, res, next) => {
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
+
   next();
 });
 
@@ -53,7 +60,7 @@ app.use(
     credentials: true,
   })
 );
-app.options("*", cors());
+// app.options("*", cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
