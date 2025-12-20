7a93ced91564a1ef53e5894c56830a3644ca018c
SONALI-Nikki <sonalipriyardarshini@gmail.com>
2025-12-20 15:13:29 +0530

    initial commit

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,6 +3,23 @@ import { login,refreshToken, logout,register  } from "../controllers/auth.contro
 
 const router = express.Router();
 router.post("/register", register);
+router.options("/login", (req, res) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://portfolio-cms-client-jr39tgxb2-sonali-nikkis-projects.vercel.app"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "POST, OPTIONS"
+  );
+  return res.sendStatus(200);
+});
+
 router.post("/login", login);
 router.get("/refresh", refreshToken);
 router.post("/logout", logout);
