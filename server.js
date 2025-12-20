152c78fe3805b2415327d4cb075be14481966335
SONALI-Nikki <sonalipriyardarshini@gmail.com>
2025-12-20 15:06:47 +0530

    initial commit

--- server.js
+++ server.js
@@ -17,6 +17,27 @@ import contactRoutes from "./src/routes/contactRoutes.js";
 
 dotenv.config();
 const app = express();
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://portfolio-cms-client-jr39tgxb2-sonali-nikkis-projects.vercel.app"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 const PORT = process.env.PORT || 5000;
 
 // Middleware
