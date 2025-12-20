1b375ce9e49c5e27db845b6ae22d8e51924467cc
SONALI-Nikki <sonalipriyardarshini@gmail.com>
2025-12-20 14:46:57 +0530

    initial commit

--- server.js
+++ server.js
@@ -23,15 +23,17 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      "https://portfolio-cms-client-sigma.vercel.app",
       "https://portfolio-cms-client-6d2b-a6xh72fgy-sonali-nikkis-projects.vercel.app",
+      "http://localhost:5173",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
-app.options("*", cors());
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
